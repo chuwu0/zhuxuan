ee6872be3019abc451842c894d5bed0b73152a2f
wangshenwei <wangshenwei@zhisland.com>
2020-12-29 14:32:01 +0800

    fix:移动端库添加完成

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'normalize.css';
+import 'lib-flexible/flexible';
 
 createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+    .use(store)
+    .use(router)
+    .mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="home">22222</div>
+    <Button>test</Button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import { Button } from 'vant';
 export default defineComponent({
     name: 'Home',
-    components: {},
+    components: {
+        Button
+    },
 });
 </script>
+
+<style lang="scss" scoped>
+.home {
+    font-size: 40px;
+    width: 40px;
+}
+</style>
